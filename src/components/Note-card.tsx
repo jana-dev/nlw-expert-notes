36ceff114d39f99a9265e41b29652081dfbd64ab
Janaina Tavares <janixgg@outlook.com>
2024-02-12 13:30:26 -0300

    Finalizado projeto notes
    Adicionado função para excluir uma nota, gravar com audio e transcrever para texto

--- src/components/Note-card.tsx
+++ src/components/Note-card.tsx
@@ -5,12 +5,15 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
     note: {
+        id: string
         date: Date
         content: string
     }
+
+    onNoteDeleted: (id:string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
     return (
         <Dialog.Root>
             <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-400 focus:ring-2 focus:ring-lime-400'>
@@ -26,7 +29,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50' />
-                <Dialog.Content className='fixed overflow-hidden left-1/2 -translate-x-1/2 -translate-y-1/2 top-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+                <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2  md:-translate-x-1/2  md:-translate-y-1/2  md:top-1/2  md:max-w-[640px] w-full  md:h-[60vh] bg-slate-700  md:ounded-md flex flex-col outline-none'>
                     <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5' />
                     </Dialog.Close>
@@ -43,6 +46,7 @@ export function NoteCard({ note }: NoteCardProps) {
                     <button
                         type='button'
                         className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+                        onClick={() => onNoteDeleted(note.id)}
                     >
                         Deseja <span className='text-red-400 group-hover:underline'>apagar essa nota</span>?
                     </button>
